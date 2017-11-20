8384ad3c348716672561ed3a95a3dd0265c728b4
matrixben <ben354164737@qq.com>
2017-11-20 23:54:44 +0800

    add database

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from flask import Flask
 from config import DevConfig
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+
+    def __init__(self, username):
+        self.username = username
+
+    def __repr__(self):
+        return "<User '{}'>".format(self.username)
 
 
 @app.route('/')
